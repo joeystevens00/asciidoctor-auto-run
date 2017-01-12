33eb6d243b5229ea20da313366c99b0de63ea8a6
jstevens00 <joeystevens00@gmail.com>
2017-01-12 17:48:28 -0500

    Move the html file to the html dir

--- bin/asciidoctor-refresh.sh
+++ bin/asciidoctor-refresh.sh
@@ -5,14 +5,26 @@ cd $DIR
 
 source "../conf/defaults.conf"
 adocfile=$1
+
+#Add the path
 adocfilepath=$adocfolder$adocfile
+
+#Remove the adoc extension
 htmlfile=$(echo $adocfile | cut -d "." -f1)
+
+#Add the path and html extension
 htmlfile=$adocfolder$htmlfile.html
 timestamp=`date +"%D_%T" | tr '/:' '-'`
 archivefile="$archive$adocfile-$timestamp"
-#Update test.html
+
+#Archive the adoc file
 cp $adocfilepath $archivefile
+
+#Generate a new HTML file
 asciidoctor $adocfilepath
+
+#Copy the HTML file to the web directory
 cp $htmlfile $webdir
 
-
+#Move the original html file to the html directory
+mv $htmlfile $htmldir

2e43478b6400a3f308685cf80df811d111d5df5f
jstevens00 <joeystevens00@gmail.com>
2017-01-12 17:39:42 -0500

    Added some checks, and more useful output

--- runme.sh
+++ runme.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
-echo "Running.. this should be the only line you see :-)"
+source "conf/defaults.conf"
+
+echo "Directory locations"
+echo "Archive Files: $archive"
+echo "Adoc Files: $adocfolder"
+echo "Config Files: $conf"
+echo "Web directory: $webdir"
+
+checkIfDirExists() {
+ if [ ! -d "$1/" ]; then
+	echo "Detected missing $1" 
+        echo "Creating the $1 directory"
+        mkdir $1
+fi
+}
+
+#Check to see if we need to create some directories
+checkIfDirExists adoc
+checkIfDirExists archive
+
+echo "Starting the monitoring script"
 bash "bin/monitor-files.sh" &
+
+#Check to see if the script is running
+monitorFilesProc=$(ps aux | grep monitor-files | grep -v "grep")
+if [ -n "$monitorFilesProc" ]; then
+	echo "Success! The script is running"
+else
+	echo "Something went wrong!"
+fi
 
